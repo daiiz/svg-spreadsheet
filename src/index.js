f0fbab04d835215b77deeb82b0163fb3dc437cf2
daiiz <da.iz.mg@gmail.com>
2020-06-22 19:57:34 +0900

    unzipできた

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const extract = require('extract-zip')
+const fsPromises = require('fs').promises
+const glob = require('glob')
+
+const clearDir = async targetDir => {
+  const files = [
+    ...glob.sync('raw/html/*.html'),
+    ...glob.sync('raw/html/*/*.css')
+  ]
+  for (const file of files) {
+    await fsPromises.unlink(file)
+  }
+}
+
+const unzip = async ({ source, targetDir }) => {
+  await clearDir(targetDir)
+  try {
+    await extract(source, { dir: targetDir })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+unzip({
+  source: 'raw/raw.zip',
+  targetDir: path.resolve('raw/html')
+})
