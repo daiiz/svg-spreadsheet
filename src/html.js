fdac67c575bea7e0ea078f5022bdcab37a537aed
daiiz <da.iz.mg@gmail.com>
2020-06-23 00:31:04 +0900

    複雑な罫線

--- src/html.js
+++ src/html.js
@@ -5,7 +5,41 @@ const parser = new DomParser()
 
 const fileOptions = { encoding: 'utf-8' }
 
-// const remove
+const sum = values => {
+  return values.reduce((a, b) => a + b)
+}
+
+const calcTableSize = table => {
+  const thead = table.getElementsByTagName('thead')[0]
+  const thList = thead.getElementsByClassName('column-headers-background')
+
+  const W = []
+  for (const th of thList) {
+    const style = th.getAttribute('style')
+    if (!style) continue
+    const [, width] = style.match(/width:\s*(\d+)px/i)
+    if (!width) continue
+    W.push(parseFloat(width))
+  }
+
+  const tbody = table.getElementsByTagName('tbody')[0]
+  const trList = tbody.getElementsByTagName('tr')
+
+  const H = []
+  for (const tr of trList) {
+    const style = tr.getAttribute('style')
+    if (!style) continue
+    const [, height] = style.match(/height:\s*(\d+)px/i)
+    if (!height) continue
+    H.push(parseFloat(height))
+  }
+
+  const borderW = 1
+  return {
+    width: sum(W) + borderW * (W.length + 1),
+    height: sum(H) + borderW * (H.length + 1)
+  }
+}
 
 const parseSpreadsheetHtml = async ({ dirPath, fileName, rootClassName }) => {
   const filePath = `${dirPath}/${fileName}`
@@ -33,16 +67,28 @@ const parseSpreadsheetHtml = async ({ dirPath, fileName, rootClassName }) => {
   if (targets.length === 0) throw new Error('Target element is not found.')
   const root = targets[0]
 
-  // 不要な要素を削除する
+  // 表の大きさを算出する
+  const table = root.getElementsByTagName('table')[0]
+  const { width, height } = calcTableSize(table)
 
+  //:first-child
   return {
     html: root.outerHTML,
     css: [
       ...resStyles,
-      // 'thead {visibility: collapse;}',
+      `.${rootClassName} {background-color: transparent;}`,
       'thead th.row-header {width: 0; border-right: 1px; width: 1px;}',
       'th.row-headers-background > div {width: 0;}',
       'thead th {height:0 !important; font-size: 0 !important;}', // XXX: 文字を消すべき
+      '.waffle td {padding: 0 3px !important;}',
+      'thead > tr > th:nth-child(2) {width: 1px !important;}', // .column-headers-background:first-child
+
+      'tbody > tr {height: 0 !important;}',
+      'tbody > tr:first-child th {height: 0 !important;}',
+      'tbody > tr:first-child th div.row-header-wrapper {line-height: 0 !important;}',
+      // borderを消したいとき
+      '.waffle td {border: 0;}',
+      '.waffle th {border: solid 1px transparent; background: transparent;}'
     ]
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ const { parseSpreadsheetHtml } = require('./html')
 const { createSvgImage } = require('./svg')
 
 const main = async () => {
-  await unzip({
-    source: 'raw/raw.zip',
-    targetDir: path.resolve('raw/html')
-  })
+  // await unzip({
+  //   source: 'raw/raw.zip',
+  //   targetDir: path.resolve('raw/html')
+  // })
 
   const { html, css } = await parseSpreadsheetHtml({
     dirPath: 'raw/html',
-    fileName: 'シート1.html',
+    fileName: '値段表.html',
     rootClassName: 'grid-container'
   })
 
   const svg = createSvgImage({
     htmlStr: html,
     styleStrs: css,
-    width: 248 - 45,
-    height: 192 - 23
+    width: 500,
+    height: 120
   })
 
   const outPath = 'out/out.svg'
